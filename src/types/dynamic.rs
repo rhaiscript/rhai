58a47e506d2e6b19117befde3a4a0679b5264101
lukasx999 <116069013+lukasx999@users.noreply.github.com>
2025-04-27 14:01:14 +0200

    code cleanup

--- src/types/dynamic.rs
+++ src/types/dynamic.rs
@@ -2875,11 +2875,7 @@ impl Dynamic {
 impl From<Option<i64>> for Dynamic {
     #[inline(always)]
     fn from(value: Option<i64>) -> Self {
-        if let Some(value) = value {
-            Self::from_int(value)
-        } else {
-            Self::UNIT
-        }
+        value.map_or(Self::UNIT, From::from)
     }
 }
 impl From<()> for Dynamic {
